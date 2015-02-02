a0c8d3c312c985bcd1db550e0b44eab5e9c0afb0
pagrawl3 <mail@prath.am>
2015-02-01 22:30:32 -0600

    fixed up the css, standardized some stuff, still a lot left

--- simwahal.js
+++ simwahal.js
@@ -3,6 +3,13 @@ var element2 = document.getElementById("wrapper");
 var element3 = document.getElementById("heading");
 var element4 = document.getElementById("wrap2.animate");
 
+
+//___##_CLICK_HANDLERS_##___//
+//click events and the functions triggered by them
+//- LIST :-
+//- # Proceed button click (#butt)
+//- # Cross button to hide overlay (#cross)
+
 document.getElementById("butt").addEventListener('click', function()
 {
 	menu.className = (menu.className == "animate")? "" :"animate";
@@ -14,6 +21,19 @@ document.getElementById("butt").addEventListener('click', function()
 		element3.className="";
 	}
 })
+
+document.getElementById("cross").addEventListener('click', function() { 
+	
+	if(cross.className == "wrap") {
+		cross.className = "";
+		wrap2.className = "";
+		element2.style.backgroundColor = "black";
+		element2.style.opacity = 0.99;
+	} else {
+		cross.className ="";
+	} 
+})
+
 
 
 var elems = document.getElementsByClassName("item");
@@ -29,16 +49,3 @@ for (i=0 ; i<6 ; i++) {
 	document.getElementById("wrap2").innerHTML = "<p class = 'text' >About Me : My name is Simrin</p> ";
 	console.log(document.getElementById("wrap2").innerHTML);
 }
-
-
-document.getElementById("cross").addEventListener('click', function() { 
-	
-	if(cross.className == "wrap") {
-		cross.className = "";
-		wrap2.className = "";
-		element2.style.backgroundColor = "black";
-		element2.style.opacity = 0.99;
-	} else {
-		cross.className ="";
-	} 
-})
